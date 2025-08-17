a8d9f4e4da242ec075be913184650dc528a4d23b
flutterbuddy1 <mayanksmind@gmail.com>
2025-08-17 20:28:26 +0530

    Adding about,contact,product page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Web_Series from "./components/WebSeries";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/404";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/contact-us" element={<Contact />} />
         <Route path="/webseries" element={<Web_Series />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
